02027260f8cbd8e364d350d4c66f263b9335439d
Ramya Nara <ramyan@infinite.com>
2024-12-22 22:17:45 +0530

    assignment-1

---
+++ ASP Assignments/WebApplication1/WebForm2.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="WebApplication1.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Product List</h1>
+            <asp:DropDownList ID="Products" runat="server" AutoPostBack="true" OnSelectedIndexChanged="Products_Selected">
+                <asp:ListItem Text="Select a product" Value="0" />
+                <asp:ListItem Text="Coke" Value="1" />
+                <asp:ListItem Text="Doll" Value="2" />
+                <asp:ListItem Text="Make Up" Value="3" />
+                <asp:ListItem Text="Shoe" Value="4" />
+            </asp:DropDownList>
+ 
+            <br /><br />
+            <asp:Image ID="imgProduct" runat="server" Width="200px" CssClass="product-image" Visible="false" />
+ 
+            <br /><br />
+            <asp:Button ID="btnGetPrice" runat="server" Text="Get Price" OnClick="btnGetPrice_Click" />
+            
+            <br /><br />
+            <asp:Label ID="labelPrice" runat="server" Text="Price: $" Visible="false" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP Assignments/WebApplication1/WebForm2.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                imgProduct.Visible = false;
+                labelPrice.Visible = false;
+            }
+        }
+
+        protected void Products_Selected(object sender, EventArgs e)
+        {
+            int choose_product = int.Parse(Products.SelectedValue);
+
+            imgProduct.Visible = false;
+            labelPrice.Visible = false;
+
+
+            switch (choose_product)
+            {
+                case 1:
+                    imgProduct.ImageUrl = "images/coke.jpg";
+                    labelPrice.Text = "Price: $200";
+                    break;
+                case 2:
+                    imgProduct.ImageUrl = "images/doll.jpg";
+                    labelPrice.Text = "Price: $500";
+                    break;
+                case 3:
+                    imgProduct.ImageUrl = "images/makeup.jpg";
+                    labelPrice.Text = "Price: $600";
+                    break;
+                case 4:
+                    imgProduct.ImageUrl = "images/shoe.jpg";
+                    labelPrice.Text = "Price: $1000";
+                    break;
+                default:
+                    imgProduct.Visible = false;
+                    labelPrice.Visible = false;
+                    break;
+            }
+
+
+            if (choose_product != 0)
+            {
+                imgProduct.Visible = true;
+            }
+        }
+
+        protected void btnGetPrice_Click(object sender, EventArgs e)
+        {
+            labelPrice.Visible = true;
+
+        }
+    }
+}
+    
+    
