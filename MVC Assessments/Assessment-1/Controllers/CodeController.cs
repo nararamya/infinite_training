6c36631d64325ffeac5e8a072eda61ad8d598de8
Ramya Nara <ramyan@infinite.com>
2025-01-02 20:01:11 +0530

    assessment1

---
+++ MVC Assessments/Assessment-1/Controllers/CodeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assessment_1.Models;
+using System.Data;
+using System.Data.Entity;
+
+
+namespace Assessment_1.Controllers
+{
+    public class CodeController : Controller
+    {
+
+        private NorthwindEntities db = new NorthwindEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CustomersResidingInGermany()
+        {
+            var customer1 = db.Customers.Where(G => G.Country == "Germany").ToList();
+            return View(customer1);
+        }
+        public ActionResult CustomerDetails()
+        {
+            var customer2 = db.Orders.Where(o => o.OrderID == 10248).Select(o => o.Customer).FirstOrDefault();
+            return View(customer2);
+        }
+    }
+}
---
+++ MVC Assessments/CodeFirst/Controllers/MoviesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodeFirst.Models;
+using CodeFirst.Repository;
+namespace CodeFirst.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly IMovieRepository movRepository;
+
+        public MoviesController()
+        {
+            movRepository = new MovieRepository();
+        }
+
+        // Display all movies
+        public ActionResult Index()
+        {
+            var movies = movRepository.GetAll();
+            return View(movies);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Create a new movie
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                movRepository.Create(movie);
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+        public ActionResult Edit(int id)
+        {
+            var movie = movRepository.GetById(id); 
+            if (movie == null)
+            {
+                return HttpNotFound(); 
+            }
+            return View(movie); 
+        }
+        // Edit a movie
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                movRepository.Edit(movie);
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+    
+        
+
+        // Display movies by year
+        public ActionResult MoviesByYear(int year)
+        {
+            var movies = movRepository.GetAllMoviesByYear(year);
+            return View(movies);
+        }
+    }
+}
